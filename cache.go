78507669553708f3d2cbcbc2ca52d5e5db568846
Evgeniy Dammer <evgeniydammer@gmail.com>
2022-05-25 10:57:01 +0500

    Add solution
    Add solution

--- cache.go
+++ cache.go
@@ -1,23 +1,54 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	cacheItems map[string]Item
+}
+
+type Item struct {
+	itemValue  string
+	itemExpire time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		cacheItems: make(map[string]Item),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache *Cache) Get(key string) (string, bool) {
+	item, ok := cache.cacheItems[key]
 
+	if ok && item.itemExpire.IsZero() {
+		return item.itemValue, true
+	} else if ok && time.Now().UTC().After(item.itemExpire) {
+		delete(cache.cacheItems, key)
+
+		return "", false
+	}
+
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.cacheItems[key] = Item{itemValue: value}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	var result []string
+
+	for key := range cache.cacheItems {
+		if _, ok := cache.Get(key); ok {
+			result = append(result, key)
+		}
+	}
+
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.cacheItems[key] = Item{itemValue: value, itemExpire: deadline}
 }
---
+++ cache_test.go
@@ -0,0 +1,224 @@
+package cache
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestNewCache(t *testing.T) {
+	testCases := []struct {
+		name string
+		want Cache
+	}{
+		{
+			name: "Constructor",
+			want: Cache{cacheItems: make(map[string]Item)},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			if got := NewCache(); !reflect.DeepEqual(got, testCase.want) {
+				t.Errorf("got: %v, want: %v", got, testCase.want)
+			}
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	testCases := []struct {
+		name  string
+		items map[string]Item
+		input string
+		want  string
+		ok    bool
+	}{
+		{
+			name: "Existed key",
+			items: map[string]Item{
+				"ABC": {
+					itemValue: "DEF",
+				},
+			},
+			input: "ABC",
+			want:  "DEF",
+			ok:    true,
+		},
+		{
+			name: "Expired key",
+			items: map[string]Item{
+				"GHI": {
+					itemValue:  "JKL",
+					itemExpire: time.Now().Add(time.Duration(-60) * time.Second),
+				},
+			},
+			input: "GHI",
+			want:  "",
+			ok:    false,
+		},
+		{
+			name:  "Not existed key",
+			items: nil,
+			input: "XYZ",
+			want:  "",
+			ok:    false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			newCache := &Cache{
+				cacheItems: testCase.items,
+			}
+
+			got, ok := newCache.Get(testCase.input)
+
+			if got != testCase.want {
+				t.Errorf("got: %v, want: %v", got, testCase.want)
+			}
+
+			if ok != testCase.ok {
+				t.Errorf("ok: %v, want: %v", ok, testCase.ok)
+			}
+		})
+	}
+}
+
+func TestPut(t *testing.T) {
+	type inp struct {
+		key   string
+		value string
+	}
+
+	testCases := []struct {
+		name  string
+		items map[string]Item
+		input inp
+		want  string
+		ok    bool
+	}{
+		{
+			name:  "Put key",
+			items: make(map[string]Item),
+			input: inp{key: "ABC", value: "DEF"},
+			want:  "DEF",
+			ok:    true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			newCache := &Cache{
+				cacheItems: testCase.items,
+			}
+
+			newCache.Put(testCase.input.key, testCase.input.value)
+
+			got, ok := newCache.Get(testCase.input.key)
+
+			if got != testCase.want {
+				t.Errorf("got: %v, want: %v", got, testCase.want)
+			}
+
+			if ok != testCase.ok {
+				t.Errorf("ok: %v, want: %v", ok, testCase.ok)
+			}
+		})
+	}
+}
+
+func TestKeys(t *testing.T) {
+	type fields struct {
+		items map[string]Item
+	}
+
+	testCases := []struct {
+		name  string
+		items map[string]Item
+		want  []string
+	}{
+		{
+			name: "Get keys",
+			items: map[string]Item{
+				"ABC": {
+					itemValue: "DEF",
+				},
+			},
+			want: []string{
+				"ABC",
+			},
+		},
+		{
+			name: "Get keys without expired",
+			items: map[string]Item{
+				"ABC": {
+					itemValue: "DEF",
+				},
+				"GHI": {
+					itemValue:  "KLM",
+					itemExpire: time.Now().Add(time.Duration(-60) * time.Second),
+				},
+			},
+			want: []string{
+				"ABC",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			newCache := &Cache{
+				cacheItems: testCase.items,
+			}
+
+			if got := newCache.Keys(); !reflect.DeepEqual(got, testCase.want) {
+				t.Errorf("got: %v, want: %v", got, testCase.want)
+			}
+		})
+	}
+}
+
+/*func TestPutTill(t *testing.T) {
+	type inp struct {
+		key      string
+		value    string
+		deadline time.Time
+	}
+
+	testCases := []struct {
+		name  string
+		items map[string]Item
+		input inp
+		want  string
+		ok    bool
+	}{
+		{
+			name:  "Put key till",
+			items: make(map[string]Item),
+			input: inp{key: "ABC", value: "DEF", deadline: time.Now().Add(36000 * time.Second)},
+			want:  "DEF",
+			ok:    true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			newCache := &Cache{
+				cacheItems: testCase.items,
+			}
+
+			newCache.PutTill(testCase.input.key, testCase.input.value, testCase.input.deadline)
+
+			got, ok := newCache.Get(testCase.input.key)
+
+			if got != testCase.want {
+				t.Errorf("got: %v, want: %v", got, testCase.want)
+			}
+
+			if ok != testCase.ok {
+				t.Errorf("ok: %v, want: %v", ok, testCase.ok)
+			}
+		})
+	}
+}*/
